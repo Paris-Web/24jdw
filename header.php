59d0c974f493ae022c2384de93253231f11344e1
Remi <remi@hteumeuleu.fr>
2014-11-30 17:22:45 +0100

    Meta viewport
    Nettoyage de la meta viewport pour un entier.

--- header.php
+++ header.php
@@ -5,7 +5,7 @@
 	<title><?php jdw_title(); ?></title>
 	<link rel="stylesheet" href="<?php bloginfo('stylesheet_url'); ?>" />
 	<link rel="alternate" type="application/rss+xml" title="Le flux RSS de <?php bloginfo('name'); ?>" href="<?php bloginfo('rss2_url'); ?>" />
-	<meta name="viewport" content="width=device-width, initial-scale=1.0" />
+	<meta name="viewport" content="width=device-width, initial-scale=1" />
 	<meta name="apple-mobile-web-app-title" content="24j de web" />
 	<?php if(is_single() || is_page()) : ?>
 	<meta property="og:title" content="<?php the_title(); ?>" />
