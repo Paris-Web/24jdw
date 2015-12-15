f91166fb276cf51984df8ea36a894c12a663eb83
Rémi Parmentier <rparmentier@tilt-studio.fr>
2015-12-15 15:58:12 +0100

    Lancement de l'appel aux dons 2015

--- donation.php
+++ donation.php
@@ -1,10 +1,9 @@
-<?php if(is_admin()) : ?>
 <?php if(!jdw_has_made_gift() && !is_page('merci')) : ?>
 <div class="donation" id="don">
 	<div class="wrap">
 		<?php get_template_part('share'); ?>
 		<div class="gift box">
-			<h3 class="box-title"><span onclick="formPaypal.submit()">Faites un don</span> pour soutenir <a href="<?php bloginfo('url'); ?>/2015/association-les-b-a-des-satellites/">l'association Les&nbsp;B-A&nbsp;des&nbsp;Satellites</a></h3>
+			<h3 class="box-title"><span onclick="formPaypal.submit()">Faites un don</span> pour soutenir <a href="<?php bloginfo('url'); ?>/2015/association-les-b-a-des-satellites/">l'association Les&nbsp;B-A&nbsp;des&nbsp;Satellites</a><br>et <a href="<?php bloginfo('url'); ?>/bundle/">recevez des e-books</a> pour tout don sup&eacute;rieur &agrave; 10&nbsp;€</h3>
 			<div class="gift-button">
 				<?php get_template_part('paypal-button'); ?>
 			</div>
@@ -30,5 +29,4 @@
 		<?php jdw_the_kitten(); ?>
 	</div>
 </div>
-<?php endif; ?>
 <?php endif; ?>
