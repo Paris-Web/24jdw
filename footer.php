7fb7ea092f09358a116ecc27ed7c01b4f4e79910
Rémi Parmentier <remi@hteumeuleu.fr>
2018-11-30 21:03:29 +0100

    Footer - Liens

--- footer.php
+++ footer.php
@@ -10,7 +10,7 @@
 					<a href="<?php bloginfo('home'); ?>" class="footer-logo"><img class="footer-logo-img" src="<?php bloginfo('template_url') ?>/images/footer/logo.png" alt="<?php get_bloginfo('name') ?>" /></a>
 				</div>
 				<div class="footer-column footer-column--text">
-					<p class="footer-baseline"><b>24 Jours de web</b>, le calendrier de l’avent des gens qui font le web d’après.</p>
+					<p class="footer-baseline"><b>24 Jours de web</b>, le calendrier de l&rsquo;avent des gens qui font le web d&rsquo;après.</p>
 					<p>2012-<?php echo date("Y"); ?>, tous droits r&eacute;serv&eacute;s.</p>
 					<?php jdw_the_illustrator(); ?>
 				</div>
@@ -19,12 +19,14 @@
 						<li><a href="<?php bloginfo('url'); ?>/a-propos/">&Agrave; Propos</a></li>
 						<li><a href="<?php bloginfo('url'); ?>/mentions-legales/">Mentions l&eacute;gales</a></li>
 						<li><a href="<?php bloginfo('url'); ?>/remerciements/">Remerciements</a></li>
+						<li><a href="https://github.com/hteumeuleu/24jdw/issues">Signaler un probl&egrave;me</a></li>
 						<li class="footer-link--twitter"><a href="https://www.twitter.com/24joursdeweb">@24joursdeweb sur Twitter</a></li>
 					</ul>
 				</div>
 				<div class="footer-archives">
 					<strong class="footer-archives-title">24 jours d&rsquo;archives</strong>
 					<ul class="footer-archives-links">
+						<li><a href="<?php bloginfo('url'); ?>/2018/">&Eacute;dition 2018</a></li>
 						<li><a href="<?php bloginfo('url'); ?>/2017/">&Eacute;dition 2017</a></li>
 						<li><a href="<?php bloginfo('url'); ?>/2016/">#nowwwel 2016</a></li>
 						<li><a href="<?php bloginfo('url'); ?>/2015/">&Eacute;dition 2015</a></li>
