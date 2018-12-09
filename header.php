180cbaf0bf346d5839fa3c41d9f2e3599ce0b60f
vvalentin-ca <vvalentin@clever-age.com>
2018-12-09 14:38:55 +0100

    Update header.php

--- header.php
+++ header.php
@@ -25,7 +25,7 @@
 		<header class="hero">
 			<div class="hero-content">
 				<h1 class="hero-h1"><a href="<?php bloginfo('home'); ?>" class="hero-logo"><?php jdw_the_logo(); ?></a></h1>
-				<p class="hero-baseline"><?php bloginfo('description'); ?></p>
+				<!--<p class="hero-baseline"><?php bloginfo('description'); ?></p>-->
 			</div>
 		</header>
 		<?php else: ?>
