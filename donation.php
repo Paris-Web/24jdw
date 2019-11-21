f4ba81cb6bc5fc990d702e19d83a6c2c43d060a2
Rémi Parmentier <remi@hteumeuleu.fr>
2019-11-21 21:01:49 +0100

    Textes encart de don

--- donation.php
+++ donation.php
@@ -5,27 +5,24 @@
 				C&rsquo;est No&euml;l, faites un don&nbsp;!
 			</h2>
 			<p class="donation-subtitle">
-				Pour soutenir l&rsquo;association <strong>&laquo;&nbsp;<a href="http://www.laubergedesmigrants.fr/">L'Auberge des Migrants</a>&nbsp;&raquo;</strong>.
+				Pour soutenir la <strong>&laquo;&nbsp;<a href="https://www.fondationpierredeniker.org">Fondation Pierre Deniker</a>&nbsp;&raquo;</strong>.
 			</p>
 		</div>
 		<div class="donation-body">
 			<div class="donation-body-column">
 				<p>
-					Depuis 2008, <a href="http://www.laubergedesmigrants.fr/">l'association l&rsquo;Auberge des Migrants</a> intervient aupr&egrave;s des exil&eacute;s dans le Calaisis et le Nord de la France, en apportant aide mat&eacute;rielle et alimentaire, accompagnement et d&eacute;fense des droits.
+					Depuis 2007, la <a href="https://www.fondationpierredeniker.org">Fondation Pierre Deniker</a> soutient la recherche et informe contre les maladies mentales. Cette ann&eacute;e, la fondation a sorti &laquo;&nbsp;<a href="https://www.youtube.com/watch?v=2YR8J1xqDNs">Et toi, ça va&nbsp;?</a>&nbsp;&raquo;, un court-m&eacute;trage illustrant la d&eacute;pression.
 				</p>
 				<p>
-					Les fonds r&eacute;colt&eacute;s vont directement sur le compte de l&rsquo;association.
+					Les fonds r&eacute;colt&eacute;s vont directement sur le compte de la fondation.
 				</p>
 			</div>
 			<div class="donation-body-column">
 				<p>
-					En cliquant sur le bouton &laquo;&nbsp;Faire un don&nbsp;&raquo; vous allez &ecirc;tre redirig&eacute; vers le site www.laubergedesmigrants.fr.
+					En cliquant sur le bouton &laquo;&nbsp;Faire un don&nbsp;&raquo; vous allez &ecirc;tre redirig&eacute; vers le site www.fondationpierredeniker.org.
 				</p>
 				<p>
-					<a class="jdw-button" href="https://www.laubergedesmigrants.fr/fr/faire-un-don/">Faire un don</a>
-				</p>
-				<p>
-					En d&eacute;cembre 2018, <a href="https://www.helloasso.com/utilisateurs/don-24joursdeweb-fr/collectes/24-jours-de-web-soutient-l-auberge-des-migrants">680&nbsp;&euro; ont &eacute;t&eacute; r&eacute;colt&eacute;s</a> pour l'Auberge des Migrants. Merci &agrave; vous&nbsp;!
+					<a class="jdw-button" href="https://www.fondationpierredeniker.org/don/">Faire un don</a>
 				</p>
 			</div>
 		</div>
