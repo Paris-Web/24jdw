4075226aa3cca85aa1d5222998d5909600b29d97
Luc Poupard <contact@kloh.ch>
2022-11-19 11:28:32 +0100

    Mise à jour du script pour les onglets

--- functions.php
+++ functions.php
@@ -87,7 +87,7 @@ function jdw_add_tabs() {
 			'tabs',
 			get_stylesheet_directory_uri().'/js/tabs.js',
 			array(),
-			'1.0.1',
+			'1.0.3',
 			false
 		);
 
--- js/tabs.js
+++ js/tabs.js
@@ -1,62 +1,96 @@
 window.addEventListener("DOMContentLoaded", () => {
-  const tabLists = document.querySelectorAll('[role="tablist"]');
+	const tabLists = document.querySelectorAll('[role="tablist"]');
 
-  tabLists.forEach((tabList) => {
-    const tabs = tabList.querySelectorAll('[role="tab"]');
-    // Add a click event handler to each tab
-    tabs.forEach((tab) => {
-      tab.addEventListener("click", changeTabs);
-    });
+	let tabPanelIndex = 0;
 
-    // Enable arrow navigation between tabs in the tab list
-    let tabFocus = 0;
+	tabLists.forEach((tabList) => {
+		const span = tabList.querySelector("span");
+		span.setAttribute("style", "display:none");
+		tabList.setAttribute("aria-labelledby", span.id);
 
-    tabList.addEventListener("keydown", (e) => {
-      // Move right
-      if (e.keyCode === 39 || e.keyCode === 37) {
-        tabs[tabFocus].setAttribute("tabindex", -1);
-        if (e.keyCode === 39) {
-          tabFocus++;
-          // If we're at the end, go to the start
-          if (tabFocus >= tabs.length) {
-            tabFocus = 0;
-          }
-          // Move left
-        } else if (e.keyCode === 37) {
-          tabFocus--;
-          // If we're at the start, move to the end
-          if (tabFocus < 0) {
-            tabFocus = tabs.length - 1;
-          }
-        }
+		const tabs = tabList.querySelectorAll('[role="tab"]');
 
-        tabs[tabFocus].setAttribute("tabindex", 0);
-        tabs[tabFocus].focus();
-      }
-    });
-  });
+		let tabIndex = 0;
+		// Add a click event handler to each tab
+		tabs.forEach((tab) => {
+			const tabPanels =
+				tabList.parentElement.parentElement.querySelectorAll(
+					'[role="tabpanel"]'
+				);
+
+			const tabPanel = tabPanels[tabPanelIndex];
+
+			if (tabIndex === 0) {
+				tab.setAttribute("aria-selected", true);
+				tab.setAttribute("tabIndex", 0);
+
+				tabPanel.setAttribute("tabIndex", 0);
+			} else {
+				tab.setAttribute("aria-selected", false);
+				tab.setAttribute("tabIndex", -1);
+
+				tabPanel.setAttribute("style", "display:none;");
+			}
+
+			tab.setAttribute("aria-controls", tabPanel.id);
+			tabPanel.setAttribute("aria-labelledby", tab.id);
+			tabPanel.setAttribute("tabIndex", 0);
+
+			tab.addEventListener("click", changeTabs);
+
+			tabIndex++;
+			tabPanelIndex++;
+		});
+
+		// Enable arrow navigation between tabs in the tab list
+		let tabFocus = 0;
+
+		tabList.addEventListener("keydown", (e) => {
+			// Move right
+			if (e.keyCode === 39 || e.keyCode === 37) {
+				tabs[tabFocus].setAttribute("tabindex", -1);
+				if (e.keyCode === 39) {
+					tabFocus++;
+					// If we're at the end, go to the start
+					if (tabFocus >= tabs.length) {
+						tabFocus = 0;
+					}
+					// Move left
+				} else if (e.keyCode === 37) {
+					tabFocus--;
+					// If we're at the start, move to the end
+					if (tabFocus < 0) {
+						tabFocus = tabs.length - 1;
+					}
+				}
+
+				tabs[tabFocus].setAttribute("tabindex", 0);
+				tabs[tabFocus].focus();
+			}
+		});
+	});
 });
 
 function changeTabs(e) {
-  const target = e.target;
-  const parent = target.parentNode;
-  const grandparent = parent.parentNode;
+	const target = e.target;
+	const parent = target.parentNode;
+	const grandparent = parent.parentNode;
 
-  // Remove all current selected tabs
-  parent
-    .querySelectorAll('[aria-selected="true"]')
-    .forEach((t) => t.setAttribute("aria-selected", false));
+	// Remove all current selected tabs
+	parent
+		.querySelectorAll('[aria-selected="true"]')
+		.forEach((t) => t.setAttribute("aria-selected", false));
 
-  // Set this tab as selected
-  target.setAttribute("aria-selected", true);
+	// Set this tab as selected
+	target.setAttribute("aria-selected", true);
 
-  // Hide all tab panels
-  grandparent
-    .querySelectorAll('[role="tabpanel"]')
-    .forEach((p) => p.setAttribute("style", "display:none;"));
+	// Hide all tab panels
+	grandparent
+		.querySelectorAll('[role="tabpanel"]')
+		.forEach((p) => p.setAttribute("style", "display:none;"));
 
-  // Show the selected panel
-  grandparent.parentNode
-    .querySelector(`#${target.getAttribute("aria-controls")}`)
-    .removeAttribute("style");
+	// Show the selected panel
+	grandparent.parentNode
+		.querySelector(`#${target.getAttribute("aria-controls")}`)
+		.removeAttribute("style");
 }
