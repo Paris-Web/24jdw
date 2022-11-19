9934dee23096df84c9ee80a0e75f143bba145874
Luc Poupard <contact@kloh.ch>
2022-11-19 14:31:04 +0100

    Amélioration Open Graph et Twitter Card

--- header.php
+++ header.php
@@ -14,16 +14,25 @@
 	<meta name="msapplication-TileColor" content="#004063">
 	<meta name="theme-color" content="#004063">
 	<meta name="apple-mobile-web-app-title" content="24j de web" />
+	<meta name="twitter:card" content="summary_large_image" />
+	<meta name="twitter:site" content="@24joursdeweb" />
 	<?php if(is_single() || is_page()) : ?>
 	<meta property="og:title" content="<?php the_title_attribute(); ?>" />
+	<meta name="twitter:title" content="<?php the_title_attribute(); ?>" />
 	<meta property="og:description" content="<?php bloginfo('name'); ?> : <?php bloginfo('description'); ?>" />
+	<meta name="twitter:description" content="<?php bloginfo('name'); ?> : <?php bloginfo('description'); ?>" />
 	<?php endif; ?>
 	<?php if((is_home() && !is_paged()) || (is_year() && get_the_time('Y') != 2016)) : ?>
 	<meta property="og:title" content="<?php bloginfo('name'); ?>" />
+	<meta name="twitter:title" content="<?php bloginfo('name'); ?>" />
 	<meta property="og:description" content="<?php bloginfo('description'); ?>" />
+	<meta name="twitter:description" content="<?php bloginfo('description'); ?>" />
 	<meta name="description" content="<?php bloginfo('description'); ?>" />
 	<?php endif; ?>
-	<meta property="og:image" content="<?php bloginfo('template_url'); ?>/images/og.png" />
+	<meta property="og:image" content="<?php bloginfo('template_url'); ?>/images/og-banner.jpg" />
+	<meta property="og:image:width" content="1200" />
+	<meta property="og:image:height" content="630" />
+	<meta name="twitter:image" content="<?php bloginfo('template_url'); ?>/images/og-banner.jpg" />
 	<?php wp_head(); ?>
 </head>
 <body <?php body_class(); ?>>
