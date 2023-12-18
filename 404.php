8debc0757358ce916ed91744872cce8b0d82490e
Gaël Poupard <gael+github@ffoodd.fr>
2023-12-18 14:37:03 +0100

    Valeur dépréciée pour l’URL vers la home dans la 404

--- 404.php
+++ 404.php
@@ -1,12 +1,12 @@
 <?php get_header(); ?>
 <div class="post">
-	<h1 class="post-title">Oups !</h1>
+	<h1 class="post-title">Oups&nbsp;!</h1>
 	<div class="entry">
 		<p>
-			Il n'y a que 24 jours dans un calendrier de l'avent. Et la page <code><?php echo $_SERVER['REQUEST_URI'] ?></code> n'en fait pas partie.
+			Il n’y a que 24 jours dans un calendrier de l’avent. Et la page <code><?php echo $_SERVER['REQUEST_URI'] ?></code> n’en fait pas partie.
 		</p>
 		<p>
-			Vous &ecirc;tes cordialement invit&eacute; &agrave; <a href="<?php bloginfo('home'); ?>">revenir &agrave; la page d'accueil</a>.
+			Vous êtes cordialement invité à <a href="<?php bloginfo('url'); ?>">revenir à la page d’accueil</a>.
 		</p>
 	</div>
 </div>
